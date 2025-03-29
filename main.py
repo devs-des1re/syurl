e41ef24ad5fbcd36f12de465fdfc7a5243032773
devs_des1re <arjunbrij8811@gmail.com>
2025-03-29 15:55:32 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+import sqldb
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        long_link = request.form["url-input"]
+
+        if long_link.strip() != '':
+            code = sqldb.create_url(long_link)
+            return redirect(url_for(f"shortened", code=code), code=302)
+        else:
+            return render_template("invalid_link.html")
+    else:
+        return render_template("index.html")
+    
+@app.route("/about-us")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/github")
+def github():
+    return render_template("github.html")
+
+@app.route("/shortened")
+def shortened():
+    code = request.args.get("code", None)
+
+    if not code:
+        return render_template("error404.html")
+    else:
+        return render_template("shortened_link.html", link=code)
+    
+@app.route("/terms-of-service")
+def tos():
+    return render_template("tos.html")
+
+@app.route("/privacy-policy")
+def privacy_policy():
+    return render_template("privacy_policy.html")
+
+@app.route("/link/<string:id>")
+def short_link(id):
+    url = sqldb.get_url(id)
+
+    if url != None:
+        return render_template("short_link.html", link=url)
+    else:
+        return render_template("invalid_link.html")
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("error404.html"), 404  
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6969)
---
+++ sqldb.py
@@ -0,0 +1,38 @@
+import sqlite3
+import random
+import string
+
+def generate_code(length=8):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def db_init():
+    db = sqlite3.connect("database.db")
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS urls (id TEXT, url TEXT)")
+
+def create_url(url):
+    while True:
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+
+        code = generate_code()
+        cursor.execute("SELECT id FROM urls WHERE id = ?", (code,))
+        data = cursor.fetchone()
+
+        if not data:
+            cursor.execute("INSERT INTO urls (id, url) VALUES (?, ?)", (code, url,))
+            db.commit()
+            return code
+
+def get_url(id):
+    db = sqlite3.connect("database.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT url FROM urls WHERE id = ?", (id,))
+    data = cursor.fetchone()
+    
+    if id:
+        return data[0]
+    else:
+        return None
+
+db_init()
